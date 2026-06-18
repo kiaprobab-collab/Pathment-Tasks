c0f70e861eb148b69206f78ed1fc6d1194a933ee
Sumeet <sadhanagupta0324@gmail.com>
2026-06-18 14:51:29 +0530

    Add: Image-Sider prj4

---
+++ Image Slider/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageSlider from './components/ImageSlider'
+
+const App = () => {
+  return (
+    <div>
+      <ImageSlider url={"https://picsum.photos/v2/list"}/>
+    </div>
+  )
+}
+
+export default App
