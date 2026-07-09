ce5075a01b756811bdff1afc12dbe4d0dc209708
Kia Probab <kiaprobab@gmail.com>
2026-07-09 18:32:34 +0530

    Add: Axios Connections

---
+++ CRUD operation/frontend/src/api/foorBallAPI.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://localhost:3001",
+    withCredentials: true,
+})
+
+export const getTeams = async()=> {
+    const {data} = await api.get("/get-teams")
+    return data;
+}
+
+export const addTeam = async(data) => {
+    const response = await api.post("/add", data)
+    return response.data;  
+}
+
+export const updateTeamName = async(id, data) => {
+    const response = await api.patch(`/update/${id}`, data)
+    return response.data;
+}
+
+export const increaseGoals = async(id) => {
+    const response = await api.patch(`/goals/increase/${id}`)
+    return response.data;
+} 
+
+export const decreaseGoals = async(id) => {
+    const response = await api.patch(`/goals/decrease/${id}`)
+    return response.data;
+} 
+
+export const deleteTeam = async(id) => {
+    const response = await api.delete(`/delete/${id}`)
+    return response.data
+}
